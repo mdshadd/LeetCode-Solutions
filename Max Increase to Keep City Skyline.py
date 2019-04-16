e3a8defb6211e66b695e742d52fae03bb4bb383e
mdshadd <shadd.op2@gmail.com>
2019-04-17 03:24:09 +0600

    Solved

--- Max Increase to Keep City Skyline.py
+++ Max Increase to Keep City Skyline.py
@@ -1,20 +1,26 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        transposeGrid = [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
+        topView = list(map(max, transposeGrid))
 
+        sideView = list(map(max, grid))
 
+        sum = 0
+        newGrid = []
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] <= sideView[i]:
+                     if sideView[i] <= topView[j]:
+                         newGrid.append(sideView[i])
+                     else:
+                         newGrid.append(topView[j])
 
+        flatGrid = []
+        for x in grid:
+            for y in x:
+                flatGrid.append(y)
 
+        for l in range(0, len(newGrid)):
+            sum += newGrid[l] - flatGrid[l]
 
-
-
-
-
-
-
-
-        # Procedures:
-        #
-        # grid = given
-        # top/bottom = 1. transpose of grid, 2. max of the rows of new matrix in a new 1D matrix
-        # left/right = Max of all rows of grid in new 1D matrix
-        # newGrid = 1. if grid[i][j] < max[j] then newGrid[i][j] = max(grid[i][j]), 2. sum += newGrid[i][j] - grid[i][j]
+        return sum
