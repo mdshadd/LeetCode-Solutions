d1f3d4f3175adb417721f5fc7d22102c48e608c7
mdshadd <shadd.op2@gmail.com>
2019-04-17 03:40:29 +0600

    To DOs

---
+++ Add Two Numbers.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+
+
+# to do:
+# 1. learn __init__ method
+# 2. understand linked List
